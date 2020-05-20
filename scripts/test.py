3741cc3915a1216d88eb2f7fbf25b27f78452e2d
Rufat Babayev <rufat.babayev@business- code.de>
2020-05-20 20:42:48 +0200

    Added tutorial for python data types

--- scripts/test.py
+++ scripts/test.py
@@ -1,2 +1,14 @@
 for i in range(10):
 	print("Hi, python")
+	
+
+import numpy as np
+
+list = [1, 2, 3]
+print(list)
+print(type(list))
+
+nplist = np.asarray(list)
+print(nplist)
+print(type(nplist))
+print(nplist.shape)
