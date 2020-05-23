fed8b65dfc828c144701060d9f9b288bdfb5043a
Rufat Babayev <rufat.babayev@business- code.de>
2020-05-23 17:39:36 +0200

    Added brian.py and brian2.py for Unix-like systems

---
+++ chapter3/brian.py
@@ -0,0 +1,4 @@
+#! /home/cavaj/anaconda3/bin/python
+print('The Bright Side ' + 'of Life...') #+ means concatenate for strings
+
+# can be executed with ./brian.py after giving executable privilege chmod +x brian.py
